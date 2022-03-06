8a226fa086dd63ca4c0f4b4d4d67963100a86ea0
phaydara <phaydara@gmail.com>
2022-03-05 20:39:11 -0600

    updated FilePondUpload component

--- src/components/FilePondUpload/FilePondUpload.jsx
+++ src/components/FilePondUpload/FilePondUpload.jsx
@@ -1,7 +1,32 @@
+import React from 'react';
+import { useState }  from 'react';
+
+// Import React FilePond
+import { FilePond, registerPlugin } from "react-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
 function FilePondUpload() {
+
+    const [files, setFiles] =useState([]);
+
     return(
-        <>
-        </>
+        <div>
+            <h2>FilePond File Upload Example</h2>
+            <FilePond 
+                allowMultiple={true}
+                files={files}
+                maxFiles={5}
+                allowReorder={true}
+            />
+        </div>
     )
 }
 
